fb3febdb0a35581575540c28f59a338e5fa4fbb5
Bisi3asi <kueeng8008@naver.com>
2024-01-25 13:30:03 +0900

    feat: ChargingStation findById 정의

---
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/service/ChargingStationService.java
@@ -0,0 +1,19 @@
+package com.ll.eitcharge.domain.chargingStation.chargingStation.service;
+
+import org.springframework.stereotype.Repository;
+
+import com.ll.eitcharge.domain.chargingStation.chargingStation.entity.ChargingStation;
+import com.ll.eitcharge.domain.chargingStation.chargingStation.repository.ChargingStationRepository;
+import com.ll.eitcharge.global.exceptions.GlobalException;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ChargingStationService {
+	private final ChargingStationRepository chargingStationRepository;
+
+	public ChargingStation findById(Long id) {
+		return chargingStationRepository.findById(id).orElseThrow(GlobalException.E404::new);
+	}
+}
