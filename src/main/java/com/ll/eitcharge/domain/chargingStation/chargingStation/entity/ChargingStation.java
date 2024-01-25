893035574e5c19562bf09b0036bb85546c974aba
Bisi3asi <kueeng8008@naver.com>
2024-01-25 13:45:42 +0900

    feat: TechnicalManger 관련 OneToOne 연관관계 설정
    * 연관관계
    - 1:1 / TechnicalManager(주인) : Member(종속)
    - 1:1 / TechnicalManager(주인) : chargingStation(종속)

--- src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/entity/ChargingStation.java
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/entity/ChargingStation.java
@@ -1,19 +1,32 @@
 package com.ll.eitcharge.domain.chargingStation.chargingStation.entity;
 
-import com.ll.eitcharge.domain.agency.agency.entity.Agency;
-import com.ll.eitcharge.domain.charger.charger.entity.Charger;
-import com.ll.eitcharge.domain.region.region.entity.Region;
-import com.ll.eitcharge.domain.report.report.entity.Report;
-import com.ll.eitcharge.domain.review.review.entity.Review;
-import jakarta.persistence.*;
-import lombok.*;
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static jakarta.persistence.FetchType.*;
-import static lombok.AccessLevel.PROTECTED;
+import com.ll.eitcharge.domain.agency.agency.entity.Agency;
+import com.ll.eitcharge.domain.charger.charger.entity.Charger;
+import com.ll.eitcharge.domain.region.region.entity.Region;
+import com.ll.eitcharge.domain.report.report.entity.Report;
+import com.ll.eitcharge.domain.review.review.entity.Review;
+import com.ll.eitcharge.domain.technicalManager.technicalManager.entity.TechnicalManager;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
@@ -25,6 +38,9 @@ public class ChargingStation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = LAZY, mappedBy = "chargingStation")
+    private TechnicalManager technicalManager;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "region_code")
