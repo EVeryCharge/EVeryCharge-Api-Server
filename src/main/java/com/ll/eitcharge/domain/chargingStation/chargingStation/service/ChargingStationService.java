7ce6820b4fee2b6b1a8b62ec229dbafa98c8c0d4
Bisi3asi <kueeng8008@naver.com>
2024-01-25 21:40:40 +0900

    feat: 엔티티 조회용 findByIdOptional 메소드 추가

--- src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/service/ChargingStationService.java
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/service/ChargingStationService.java
@@ -1,6 +1,9 @@
 package com.ll.eitcharge.domain.chargingStation.chargingStation.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ll.eitcharge.domain.chargingStation.chargingStation.entity.ChargingStation;
 import com.ll.eitcharge.domain.chargingStation.chargingStation.repository.ChargingStationRepository;
@@ -10,10 +13,25 @@ import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChargingStationService {
 	private final ChargingStationRepository chargingStationRepository;
 
 	public ChargingStation findById(Long id) {
 		return chargingStationRepository.findById(id).orElseThrow(GlobalException.E404::new);
+	}
+	// temp method for execute test
+	public Optional<ChargingStation> findByIdOptional(Long id) {
+		return chargingStationRepository.findById(id);
+	}
+
+	// temp method for execute test
+	@Transactional
+	public void create(String name) {
+		ChargingStation chargingStation = ChargingStation.builder()
+			.name(name)
+			.build();
+
+		chargingStationRepository.save(chargingStation);
 	}
 }
