00ba7cd0ec9e87069d9578ac9a2a11c13476140b
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-01-31 00:29:13 +0900

    feat: 테이블 UI변경

--- front/src/components/ChargingStationStateTable.js
+++ front/src/components/ChargingStationStateTable.js
@@ -2,20 +2,36 @@ import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 
+const ChargerType = {
+  "01":"DC차데모",
+  "02":"AC완속",
+  "03":"DC차데모+AC3상",
+  "04":"DC콤보",
+  "05":"DC차데모+DC콤보",
+  "06":"DC차데모+AC상+DC콤보",
+  "07":"AC상",
+  "08":"DC콤보(완속)"
+}
 
+const ChargerState = {
+  "1":"통신이상",
+  "2":"충전대기",
+  "3":"충전중",
+  "4":"운영중지",
+  "5":"점검중",
+  "9":"상태미확인"
+}
 
 
 const ChargingStationStateTable = ({statId}) => {
     const[chargingStationData, setChargingStationData] = useState([]);
-    
-
     useEffect(() => {
         // Axios를 사용하여 데이터를 가져옵니다.
         //임시데이터
         statId = "ME19A278";
-        const url = `http://localhost:8090/station/${statId}/chargers`;
+        const url = `http://localhost:8090/api/v1/chargingStation/${statId}/chargers`;
         console.log(url);
-        axios.get(`http://localhost:8090/station/${statId}/chargers`)
+        axios.get(url, {withCredentials: true})
           .then((response) => {
             // 요청이 성공하면 데이터를 상태에 저장합니다.
             console.log(response);
@@ -36,19 +52,19 @@ const ChargingStationStateTable = ({statId}) => {
       <Table>
         <TableHead>
           <TableRow>
-          <TableCell>충전기 ID</TableCell>
-            <TableCell>충전기 상태</TableCell>
             <TableCell>충전기 타입</TableCell>
-            <TableCell>마지막 충전 시간</TableCell>
+            <TableCell>충전기 상태</TableCell>
+            <TableCell>충전기 용량</TableCell>
+            <TableCell>이용가능 시간</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {chargingStationData.map((row, index) => (
             <TableRow key={index}>
-              <TableCell>{row.chgerId}</TableCell>
-              <TableCell>{row.stat}</TableCell>
-              <TableCell>{row.chgerType}</TableCell>
-              <TableCell>{row.lastTsdt}</TableCell>
+              <TableCell>{ChargerType[row.chgerType]}</TableCell>
+              <TableCell>{ChargerState[row.stat]}</TableCell> 
+              <TableCell>{row.output + "kW"}</TableCell>
+              <TableCell>{row.useTime}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/controller/ChargingStationController.java
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/controller/ChargingStationController.java
@@ -1,22 +1,43 @@
 package com.ll.eitcharge.domain.chargingStation.chargingStation.controller;
 
-import com.ll.eitcharge.domain.chargingStation.chargingStation.service.ChargingStationApiService;
+import com.ll.eitcharge.domain.chargingStation.chargingStation.dto.ChargingStationSearchResponseDto;
+import com.ll.eitcharge.domain.chargingStation.chargingStation.entity.ChargingStation;
+import com.ll.eitcharge.domain.chargingStation.chargingStation.service.ChargingStationService;
 import com.ll.eitcharge.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping(value = "/api/v1/chargingStation", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "ChargingStationController", description = "충전소 컨트롤러 API")
 @RequiredArgsConstructor
-public class
-ChargingStationController {
+public class ChargingStationController {
 
-    private final ChargingStationApiService chargingStationApiService;
+    private final ChargingStationService chargingStationService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChargingStation> get(String id) {
+        return ResponseEntity.ok(chargingStationService.findById(id));
+    }
 
 
-    @GetMapping("/station/{stationId}/chargers")
+    @GetMapping("/location/search")
+    public ResponseEntity<List<ChargingStationSearchResponseDto>> searchStation(
+            @RequestParam double swLat,
+            @RequestParam double swLng,
+            @RequestParam double neLat,
+            @RequestParam double neLng
+    ) {
+        return ResponseEntity.ok(chargingStationService.findByLatBetweenAndLngBetween(swLat, swLng, neLat, neLng));
+    }
+
+    @GetMapping("/{stationId}/chargers")
     public RsData< Object > chargerState(@PathVariable("stationId") String stationId){
-          return chargingStationApiService.findFromApi3(stationId);
+        return chargingStationService.findFromApi(stationId);
     }
 }
