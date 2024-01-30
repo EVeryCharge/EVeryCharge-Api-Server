553111fb419e77d102b2db1c1c7579bf9778bd40
Bisi3asi <kueeng8008@naver.com>
2024-01-31 02:03:48 +0900

    fix: 글 작성, 수정 화면 이전버튼 클릭 시 confirm 작동

--- front/src/components/GoBackButton.js
+++ front/src/components/GoBackButton.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 
-const GoBackButton = () => {
+const GoBackButton = ({ isEditPage }) => {
   const navigate = useNavigate();
 
-  const goBack = ({ isEditPage }) => {
+  const goBack = () => {
     if (isEditPage) {
       const confirmMessage =
-        "작성 중인 내용은 저장되지 않습니다. 정말로 이전 화면으로 이동하시겠습니까?";
+        "작성 중인 내용은 저장되지 않습니다.\n정말로 이전 화면으로 이동하시겠습니까?";
       const isConfirmed = window.confirm(confirmMessage);
 
       if (isConfirmed) {
-        // 여기서 작성하던 내용을 저장하지 않고 이전 페이지로 이동하는 로직을 추가할 수 있습니다.
         navigate(-1);
       }
     } else {
