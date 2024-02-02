98a92fd20277870d80219b377e25180406e290c2
jaeuklim <jeuk007@skuniv.ac.kr>
2024-02-02 21:43:52 +0900

    feat : 페이징 기능 추가

--- src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/controller/InquiryController.java
+++ src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/controller/InquiryController.java
@@ -1,7 +1,36 @@
 package com.ll.eitcharge.domain.inquiry.inquiry.controller;
 
+import com.ll.eitcharge.domain.inquiry.inquiry.dto.InquiryResponseDto;
+import com.ll.eitcharge.domain.inquiry.inquiry.service.InquiryService;
+import com.ll.eitcharge.domain.report.report.dto.ReportResponseDto;
+import com.ll.eitcharge.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+
+@RestController
+@RequestMapping(value = "/api/v1/inquiry", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "InquiryController", description = "문의게시판 컨트롤러 API")
+@RequiredArgsConstructor
 public class InquiryController {
+    private final InquiryService inquiryService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<InquiryResponseDto>> getList(
+            @RequestParam(value="page", defaultValue = "0") int page,
+            @RequestParam(value="pageSize", defaultValue = "10") int pageSize
+    ) {
+        Page<InquiryResponseDto> inquiryList = inquiryService.getList(page, pageSize);
+        return ResponseEntity.ok(inquiryList);
+    }
+
 }
--- src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/service/InquiryService.java
+++ src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/service/InquiryService.java
@@ -1,7 +1,35 @@
 package com.ll.eitcharge.domain.inquiry.inquiry.service;
 
+import com.ll.eitcharge.domain.inquiry.inquiry.dto.InquiryResponseDto;
+import com.ll.eitcharge.domain.inquiry.inquiry.entity.Inquiry;
+import com.ll.eitcharge.domain.inquiry.inquiry.repository.InquiryRepository;
+import com.ll.eitcharge.domain.report.report.dto.ReportResponseDto;
+import com.ll.eitcharge.domain.report.report.entity.Report;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class InquiryService {
+    private final InquiryRepository inquiryRepository;
+
+    public Page<InquiryResponseDto> getList(int page, int pageSize){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sorts));
+
+        Page<Inquiry> inquiryPage = inquiryRepository.findAll(pageable);
+
+        return inquiryPage.map(InquiryResponseDto::new);
+    }
 }
