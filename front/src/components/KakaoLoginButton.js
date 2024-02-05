2e74d9e9466ca70916fc62578d559e845e019c57
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-05 09:57:50 +0900

    feat: kakao로그인 버튼

---
+++ front/src/components/KakaoLoginButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const KakaoLoginButton = () =>{
+    const redirectUrl = "http://localhost:3000";
+
+  // 클릭 이벤트 핸들러
+  const handleClick = () => {
+    window.location.href = `http://localhost:8090/socialLogin/kakao?redirectUrl=${encodeURIComponent(redirectUrl)}`;
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>카카오 로그인</button>
+    </div>
+  );
+}
+
+export default KakaoLoginButton;
