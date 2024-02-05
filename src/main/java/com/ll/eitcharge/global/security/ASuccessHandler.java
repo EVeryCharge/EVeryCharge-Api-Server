c4a46bd6d419204cde53e77d5bc99ac3d53363c5
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-05 09:56:09 +0900

    feat: successhandler 추가

---
+++ src/main/java/com/ll/eitcharge/global/security/ASuccessHandler.java
@@ -0,0 +1,80 @@
+package com.ll.eitcharge.global.security;
+
+import com.ll.eitcharge.domain.member.member.entity.Member;
+import com.ll.eitcharge.domain.member.member.service.AuthTokenService;
+import com.ll.eitcharge.domain.member.member.service.MemberService;
+import com.ll.eitcharge.global.rq.Rq;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class ASuccessHandler implements AuthenticationSuccessHandler {
+
+    private final AuthTokenService authTokenService;
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @Value("${custom.dev.frontUrl}")
+    private String frontUrl;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        Member member = memberService.findByUsername(authentication.getName()).get();
+        String accessToken = authTokenService.genAccessToken(member);
+        String refreshToken = member.getRefreshToken();
+
+
+        String redirectUrlAfterSocialLogin = rq.getCookieValue("redirectUrlAfterSocialLogin", "");
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", accessToken)
+                .path("/")
+                .domain("localhost")
+                .sameSite("Strict")
+                .secure(true)
+                .httpOnly(true)
+                .build();
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .path("/")
+                .domain("localhost")
+                .sameSite("Strict")
+                .secure(true)
+                .httpOnly(true)
+                .build();
+
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+
+        Cookie[] cookies = request.getCookies();
+
+        Cookie cookie1 = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(redirectUrlAfterSocialLogin)) {
+                cookie1 = cookie;
+            }
+        }
+
+
+        if (cookie1 != null) {
+            cookie1.setPath("/");
+            cookie1.setMaxAge(0);
+            response.addCookie(cookie1);
+        }
+
+        response.sendRedirect(frontUrl);
+    }
+}
