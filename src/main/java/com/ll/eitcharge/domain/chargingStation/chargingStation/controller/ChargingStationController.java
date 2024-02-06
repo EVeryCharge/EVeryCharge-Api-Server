3be4f7bff2f090996016554f3bdc5f99d7a3007d
Bisi3asi <kueeng8008@naver.com>
2024-02-06 17:18:41 +0900

    feat: 초기 검색 화면 진입 시 메뉴 아이템 GET API 정의

--- src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/controller/ChargingStationController.java
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/controller/ChargingStationController.java
@@ -2,15 +2,8 @@ package com.ll.eitcharge.domain.chargingStation.chargingStation.controller;
 
 import static org.springframework.util.MimeTypeUtils.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.ll.eitcharge.domain.chargingStation.chargingStation.dto.ChargingStationSearchResponseDtoWithExecuteTime;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ll.eitcharge.domain.chargingStation.chargingStation.dto.ChargingStationSearchItemResponseDto;
 import com.ll.eitcharge.domain.chargingStation.chargingStation.dto.ChargingStationSearchResponseDto;
+import com.ll.eitcharge.domain.chargingStation.chargingStation.dto.ChargingStationSearchResponseDtoWithExecuteTime;
 import com.ll.eitcharge.domain.chargingStation.chargingStation.entity.ChargingStation;
 import com.ll.eitcharge.domain.chargingStation.chargingStation.service.ChargingStationService;
 import com.ll.eitcharge.global.rsData.RsData;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -32,63 +28,65 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChargingStationController {
 
-    private final ChargingStationService chargingStationService;
+	private final ChargingStationService chargingStationService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ChargingStation> get(String id) {
-        return ResponseEntity.ok(chargingStationService.findById(id));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<ChargingStation> get(String id) {
+		return ResponseEntity.ok(chargingStationService.findById(id));
+	}
 
-    @GetMapping("/location/search")
-    public ResponseEntity<List<ChargingStationSearchResponseDto>> searchStation(
-            @RequestParam double swLat,
-            @RequestParam double swLng,
-            @RequestParam double neLat,
-            @RequestParam double neLng
-    ) {
-        return ResponseEntity.ok(chargingStationService.findByLatBetweenAndLngBetween(swLat, swLng, neLat, neLng));
-    }
+	@GetMapping("/location/search")
+	public ResponseEntity<List<ChargingStationSearchResponseDto>> searchStation(
+		@RequestParam double swLat,
+		@RequestParam double swLng,
+		@RequestParam double neLat,
+		@RequestParam double neLng
+	) {
+		return ResponseEntity.ok(chargingStationService.findByLatBetweenAndLngBetween(swLat, swLng, neLat, neLng));
+	}
 
-    @GetMapping("/{stationId}/chargers")
-    public RsData< Object > chargerState(@PathVariable("stationId") String stationId){
-        return chargingStationService.findFromApi(stationId);
-    }
+	@GetMapping("/{stationId}/chargers")
+	public RsData<Object> chargerState(@PathVariable("stationId") String stationId) {
+		return chargingStationService.findFromApi(stationId);
+	}
 
-    @Operation(summary = "충전소 검색", description = "키워드 단위 충전소 검색 (Param)")
-    @GetMapping("/search")
-    public ResponseEntity<ChargingStationSearchResponseDtoWithExecuteTime> list(
-            // 개방 여부 (Y / N)
-            @RequestParam(value = "limitYn", defaultValue = "") String limitYn,
+	@GetMapping("/search/item")
+	public ResponseEntity<ChargingStationSearchItemResponseDto> getSearchMenuBaseItem() {
+		return ResponseEntity.ok(chargingStationService.getSearchMenuBaseItem());
+	}
 
-            // 무료 주차 (Y / N)
-            @RequestParam(value = "parkingFree", defaultValue = "") String parkingFree,
+	@GetMapping("/search/region")
+	public ResponseEntity<ChargingStationSearchItemResponseDto> getSearchMenuRegionDetailItem(
+		@RequestParam(value = "zcode") String zcode
+	) {
+		return ResponseEntity.ok(chargingStationService.getSearchMenuRegionDetailItem(zcode));
+	}
 
-            // 지역 단위 코드 (ex. 서울시 : 11 경기도 : 41 ... ), TODO 현위치 로직 구현
-            @RequestParam(value = "zcode", defaultValue = "") String zcode,
-
-            // 지역 세부단위 코드 (ex. 종로구 : 11110, 서구 : 41130 ...)
-            @RequestParam(value = "zscode", defaultValue = "") String zscode,
-
-            // 상위 주요 기관 여부 (Y: 점유율 80% 상위 15개 기관 소속 충전소, N : 하위 기타 기관 소속 충전소)
-            @RequestParam(value = "isPrimary", defaultValue = "") String isPrimary,
-
-            // 기관 코드 (ex. 차지비 : PI)
-            @RequestParam(value = "busiId", defaultValue = "") List<String> busiIds,
-
-            // 보유 충전기 타입 (01 ~ 08)
-            @RequestParam(value = "chgerType", defaultValue = "") List<String> chgerTypes,
-
-            // 검색 키워드 (충전소명, 주소 LIKE)
-            @RequestParam(value = "kw", defaultValue = "") String kw,
-
-            // 페이지 정보 (1부터 시작)
-            @RequestParam( defaultValue = "1") int page,
-
-            // 페이지 사이즈
-            @RequestParam( defaultValue = "20") int pageSize
-    ){
-        return ResponseEntity.ok(chargingStationService.search(
-                limitYn, parkingFree, zcode, zscode, isPrimary, busiIds, chgerTypes, kw, page, pageSize));
-    }
-
+	@Operation(summary = "충전소 검색", description = "키워드 단위 충전소 검색 (Param)")
+	@GetMapping("/search")
+	public ResponseEntity<ChargingStationSearchResponseDtoWithExecuteTime> list(
+		// 개방 여부 (Y / N)
+		@RequestParam(value = "limitYn", defaultValue = "") String limitYn,
+		// 무료 주차 (Y / N)
+		@RequestParam(value = "parkingFree", defaultValue = "") String parkingFree,
+		// 지역 단위 코드 (ex. 서울시 : 11 경기도 : 41 ... )
+		@RequestParam(value = "zcode", defaultValue = "") String zcode,
+		// 지역 세부단위 코드 (ex. 종로구 : 11110, 서구 : 41130 ...)
+		@RequestParam(value = "zscode", defaultValue = "") String zscode,
+		// 상위 주요 기관 여부 (Y: 점유율 80% 상위 15개 기관 소속 충전소, N : 하위 기타 기관 소속 충전소)
+		@RequestParam(value = "isPrimary", defaultValue = "") String isPrimary,
+		// 기관 코드 (ex. 차지비 : PI)
+		@RequestParam(value = "busiId", defaultValue = "") List<String> busiIds,
+		// 보유 충전기 타입 (01 ~ 08)
+		@RequestParam(value = "chgerType", defaultValue = "") List<String> chgerTypes,
+		// 검색 키워드 (충전소명, 주소 LIKE)
+		@RequestParam(value = "kw", defaultValue = "") String kw,
+		// 페이지 정보 (1부터 시작)
+		@RequestParam(defaultValue = "1") int page,
+		// 페이지 사이즈
+		@RequestParam(defaultValue = "20") int pageSize
+	) {
+		return ResponseEntity.ok(chargingStationService.search(
+			limitYn, parkingFree, zcode, zscode, isPrimary, busiIds, chgerTypes, kw, page, pageSize));
+	}
 }
