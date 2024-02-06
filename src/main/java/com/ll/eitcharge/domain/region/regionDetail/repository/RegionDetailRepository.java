e20469123281848c295221e14cf68ababcd1cc88
Bisi3asi <kueeng8008@naver.com>
2024-02-06 17:17:53 +0900

    fix: zcode 기준 세부지역 엔티티 리스트 조회 메소드 수정

--- src/main/java/com/ll/eitcharge/domain/region/regionDetail/repository/RegionDetailRepository.java
+++ src/main/java/com/ll/eitcharge/domain/region/regionDetail/repository/RegionDetailRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ll.eitcharge.domain.region.regionDetail.entity.RegionDetail;
 
 @Repository
 public interface RegionDetailRepository extends JpaRepository<RegionDetail, String> {
-	@Query("SELECT rd FROM RegionDetail rd WHERE rd.zcode = :zcode")
-	List<RegionDetail> findByFkZcode(String zcode);
+	@Query("SELECT rd FROM RegionDetail rd WHERE rd.zcode.zcode = :zcode")
+	List<RegionDetail> findByFkZcode(@Param("zcode") String zcode);
 }
