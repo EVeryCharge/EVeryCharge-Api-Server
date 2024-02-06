95c568e7b336c4db20e5b3687ae1e17101abc1e7
Bisi3asi <kueeng8008@naver.com>
2024-02-07 01:15:29 +0900

    feat: 검색 아이템 컨테이너 추가

---
+++ front/src/components/StationSearchContainer.js
@@ -0,0 +1,8 @@
+import { Card } from "@material-ui/core";
+import React from "react";
+
+const StationSearchContainer = () => {
+  return <Card variant="outlined">{/* 추가 예정 */}</Card>;
+};
+
+export default StationSearchContainer;
