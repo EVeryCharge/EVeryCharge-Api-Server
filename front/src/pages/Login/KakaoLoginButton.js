92a210e2ad504624d9fd4f667c91e41233b97b90
SeoungminKwon <149944467+SeoungminKwon@users.noreply.github.com>
2024-02-07 14:44:38 +0900

    카카오 로그인 리다렉션 오류, 회원가입, 로그인 UI 개선, 회원가입 오류 (#90)
    * feat: extractDomainFromFrontUrl메서드 추가
    
    * feat: kakao로그인 버튼 UI 개선
    
    * feat: 로그인 페이지 UI 개선
    
    * feat: 회원가입 페이지 UI 개선
    
    * fix: 회원가입 오류

--- front/src/pages/Login/KakaoLoginButton.js
+++ front/src/pages/Login/KakaoLoginButton.js
@@ -1,14 +1,25 @@
 import React from "react";
+import {Button} from '@material-ui/core';
+import kakaoLoginImage from '../../assets/kakao_login.png';
 
 const KakaoLoginButton = () =>{
+  const url = `${process.env.REACT_APP_BACKEND_URL}/socialLogin/kakao?redirectUrl=${process.env.REACT_APP_FRONTEND_URL}`;
+  console.log(url);
+
   // 클릭 이벤트 핸들러
-  const handleClick = () => {
-    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/socialLogin/kakao?redirectUrl=${encodeURIComponent(process.env.REACT_APP_FRONTEND_URL)}`;
-  };
+  // const handleClick = () => {
+   
+  //   window.location.href = `${process.env.REACT_APP_BACKEND_URL}/socialLogin/kakao?redirectUrl=${encodeURIComponent(process.env.REACT_APP_FRONTEND_URL)}`;
+  // };
   return (
-    <div>
-      <button onClick={handleClick}>카카오 로그인</button>
-    </div>
+    <Button
+      style={{ marginTop: '20px', backgroundColor: '#FFEB00', color: 'black', padding: 0, minWidth: 'auto' }} // 버튼 스타일 수정
+      variant="contained"
+      fullWidth
+      href={url}
+    >
+      <img src={kakaoLoginImage} alt="카카오 로그인" style={{ width: '40%', height: 'auto' }} /> 
+    </Button>
   );
 }
 
