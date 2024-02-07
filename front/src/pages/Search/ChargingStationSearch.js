38257a088847d4709f1187cf6e97b3e97cfcab3e
Bisi3asi <kueeng8008@naver.com>
2024-02-07 10:16:34 +0900

    remove: 충전소 검색 관련 컴포넌트 페이지로 이동

--- front/src/pages/Search/ChargingStationSearch.js
+++ front/src/pages/Search/ChargingStationSearch.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react';
-import axios from 'axios';
-import ChargerInfoModal from './modal/ChargerInfoModal';
-import { debounce } from 'lodash';
-import { HttpGet, HttpPost } from '../services/HttpService';
-const SearchMap = () => {
+import { Box } from "@material-ui/core";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import ChargingStationSearchBar from "./ChargingStationSearchBar";
+
+const ChargingStationSearch = () => {
   const mapRef = useRef(null);
   let map; // 지도 객체를 담을 변수
-  // const map = useRef(null);  
-  const [mapCenter, setMapCenter] = useState({ lat: 36.39213160000001, lng: 127.02977109999999 });
-
+  // const map = useRef(null);
+  const [mapCenter, setMapCenter] = useState({
+    lat: 36.39213160000001,
+    lng: 127.02977109999999,
+  });
 
   const initMap = () => {
     const container = document.getElementById("map");
     const options = {
       center: new window.kakao.maps.LatLng(mapCenter.lat, mapCenter.lng),
-      level: 3
+      level: 3,
     };
 
     map = new window.kakao.maps.Map(container, options);
-  }
+  };
 
   useMemo(() => {
     if (navigator.geolocation) {
@@ -28,49 +29,45 @@ const SearchMap = () => {
     function success(position) {
       setMapCenter({
         latitude: position.coords.latitude,
-        longitude: position.coords.longitude
+        longitude: position.coords.longitude,
       });
     }
 
     function error() {
       setMapCenter({
         latitude: 36.483034,
-        longitude: 126.902435
+        longitude: 126.902435,
       });
       console.log("위치 받기 실패");
     }
   }, [navigator.geolocation.getCurrentPosition]);
 
-
   useEffect(() => {
-    window.kakao.maps.load(()=>initMap());
+    window.kakao.maps.load(() => initMap());
   }, [mapRef]);
 
-
-
   return (
-    <div >
-
-
+    <Box sx={{ flexWrap: "wrap" }}>
+      <ChargingStationSearchBar />
       <div
         id="map"
         style={{
-          marginTop: '20px',
-          width: '70%',
-          height: 'calc(100vh - 165px)',
-          border: '1px solid #ccc',
-          overflow: 'hidden'
+          marginTop: "20px",
+          width: "70%",
+          height: "calc(100vh - 165px)",
+          border: "1px solid #ccc",
+          overflow: "hidden",
         }}
         ref={mapRef}
-        
       />
       <button
         style={{ position: "relative", zIndex: "2" }}
         onClick={() => initMap}
-      >위치 조정</button>
-
-    </div>
+      >
+        위치 조정
+      </button>
+    </Box>
   );
-}
+};
 
-export default SearchMap;
+export default ChargingStationSearch;
--- front/src/pages/Search/ChargingStationSearchBar.js
+++ front/src/pages/Search/ChargingStationSearchBar.js
@@ -1,8 +1,8 @@
 import { Card } from "@material-ui/core";
 import React from "react";
 
-const StationSearchBar = () => {
+const ChargingStationSearchBar = () => {
   return <Card variant="outlined">{/* 추가 예정 */}</Card>;
 };
 
-export default StationSearchBar;
+export default ChargingStationSearchBar;
