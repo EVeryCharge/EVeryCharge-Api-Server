ebd0455d10d428ba36c4e61cdfd0cbf69de28668
Bisi3asi <kueeng8008@naver.com>
2024-02-07 09:29:33 +0900

    rename: StationSearchContainer → StationSearchBar

--- front/src/components/StationSearchBar.js
+++ front/src/components/StationSearchBar.js
@@ -1,8 +1,8 @@
 import { Card } from "@material-ui/core";
 import React from "react";
 
-const StationSearchContainer = () => {
+const StationSearchBar = () => {
   return <Card variant="outlined">{/* 추가 예정 */}</Card>;
 };
 
-export default StationSearchContainer;
+export default StationSearchBar;
