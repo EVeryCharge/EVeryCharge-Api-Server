ffd97f677b5ecc5feeebb26c58762d2fd5fe0612
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-07 11:02:42 +0900

    feat: kakao로그인 버튼 UI 개선

--- front/src/components/KakaoLoginButton.js
+++ front/src/components/KakaoLoginButton.js
@@ -1,20 +1,29 @@
 import React from "react";
+import { HttpGet } from "../services/HttpService";
+import {Button} from '@material-ui/core';
+import kakaoLoginImage from '../assets/kakao_login.png';
 
 const KakaoLoginButton = () =>{
   
-  const redirectUrl = "http://localhost:3000";
+  const url = `${process.env.REACT_APP_BACKEND_URL}/socialLogin/kakao?redirectUrl=${process.env.REACT_APP_FRONTEND_URL}`;
   
 
   
   // 클릭 이벤트 핸들러
   const handleClick = () => {
+   
     window.location.href = `${process.env.REACT_APP_BACKEND_URL}/socialLogin/kakao?redirectUrl=${encodeURIComponent(process.env.REACT_APP_FRONTEND_URL)}`;
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>카카오 로그인</button>
-    </div>
+    <Button
+      style={{ marginTop: '20px', backgroundColor: '#FFEB00', color: 'black', padding: 0, minWidth: 'auto' }} // 버튼 스타일 수정
+      variant="contained"
+      fullWidth
+      onClick={handleClick}
+    >
+      <img src={kakaoLoginImage} alt="카카오 로그인" style={{ width: '40%', height: 'auto' }} /> 
+    </Button>
   );
 }
 
