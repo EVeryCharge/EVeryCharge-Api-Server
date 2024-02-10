4702a7a5558871737ccd565e64e4d38420322156
i-am-jiwon <dlark1128@gmail.com>
2024-02-10 23:13:49 +0900

    feat : front) 지도 리스트 처음으로 이동, 내위치로 이동 기능 구현 완료

--- front/src/pages/Search/ChargingStationSearch.js
+++ front/src/pages/Search/ChargingStationSearch.js
@@ -8,11 +8,38 @@ const ChargingStationSearch = () => {
   // useState로 searchParam에 대한 변화 감지
   const [searchResult, setSearchResult] = useState(null);
   const [userLat, setUserLat] = useState(null);
-  const [userLng, setUserLng] = useState(null);
+  const [userLng, setUserLng] = useState([null]);
+  
+  //지도를 위해 추가
+  const [temporaryArray, setTemporaryArray] = useState([]);
+  const [myLoc, setMyLoc] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+    function success(position) {
+      setMyLoc({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+      console.log(myLoc);
+    }
+
+    function error() {
+      console.log("너");
+    }
+  }, []);
 
   useEffect(() => {
     console.log("검색 결과(리스트): ", searchResult);
+    // 검색 결과가 변경될 때마다 temporaryArray 업데이트
+    if (searchResult) {
+      setTemporaryArray(searchResult); // 검색 결과를 temporaryArray에 저장
+    }
   }, [searchResult]);
+  // 추가 끝
 
   const fetchSearchResult = async (searchParam) => {
     try {
@@ -44,7 +71,7 @@ const ChargingStationSearch = () => {
         <ChargingStationSearchBar onSearch={fetchSearchResult} />
       </Box>
       <Box sx={{ flexGrow: 1 }}>
-        <ChargingStationSearchMap />
+        <ChargingStationSearchMap temporaryArray={temporaryArray} myLoc={myLoc} />
       </Box>
     </Box>
   );
