598c65e625e044d8a31cb67f5c5aa42c4589e660
SeoungminKwon <149944467+SeoungminKwon@users.noreply.github.com>
2024-02-13 23:57:14 +0900

    Feature/inquiry board (#104)
    * feat : 1대1문의 버튼 상단 바에 추가 및 상세 페이지 틀 구현
    
    * feat : 1대1문의 백엔드 개발 필수 파일 추가.
    
    * feat : 필요 Entity 추가 및 ResponseDto 작업
    
    * feat : 페이징 기능 추가
    
    * feat : InquirtRequestDto 추가
    
    * feat : 글쓰기 기능 추가 및 세부 로직 구현
    
    * fix : inquiry 엔터티 연관관계 버그 수정
    
    * fix : axios에서 http로 요청방식이 바뀌면서 일부 로직이 비정상작동하던 부분을 수정
    
    * fix : null 데이터에 대한 유효성 검증이 이루어지지 않았던 버그에 대해 수정
    
    * feat : 1대1문의 글쓰기 페이지 추가
    
    * fix : 공개글 여부 값이 잘 저장되지 않았던 버그 수정 및 변수명 오타 수정
    
    * fix : 서버로부터 전송받은 fetch data가 세팅되지 않았던 버그  수정 및 글 목록 리스트 수정
    
    * fix : 글 생성 시간이 responsedto에 저장되어있지 않은 부분 수정
    
    * fix : 파일 경로가 변경되어 import 오류가 났던 부분 경로 수정
    
    * fix : 글쓰기 버튼이 보이지 않아 상단으로 임시 이동
    
    * feat: 1대1문의글 상세보기 기능 추가.
    
    * feat: 1대1문의글 댓글 entity추가
    
    * fix : param값이 url에 들어가지 않는 버그 수정
    
    * design : 글 상세보기 페이지 UI 디자인 개선
    
    * feat : 문의 글 수정 및 삭제 api 및 기능 추가
    
    * feat : front 문의 글 수정 폼 추가
    
    * fix : 문의글 수정시 공개 여부가 잘 저장되지 않았던 버그 수정
    
    * fix : 자신의 게시글이 아니어도 수정 폼 이동 및 삭제 버튼이 활성화 되어있는 문제 수정
    
    * feat : 게시글 조회시 조회수 증가 기능 추가 + 불필요한 로그 코드 삭제
    
    * feat: InquiryComment UI
    
    * feat: commentRequestDTO
    
    * feat: commentResponseDTO
    
    * feat: comment 엔티티 추가
    
    * feat: InquiryEntity에 comment 필드 추가, 연관관계 매핑
    
    * rename: comment엔티티 위치 변경
    
    * remove: 기존 InquiryComment.js 파일 삭제
    
    * feat: CommentModifyRequestDto,CommentModifyResponseDto
    
    * feat: CommentController
    
    * feat: CommentRepository
    
    * feat: 댓글 컴포넌트 추가
    
    * feat: CommentService
    
    * feat: InquiryComment
    
    * feat: InquiryComment수정 폼 컴포넌트
    
    * feat: api 주소 변경
    
    * feat: 권한체크
    
    * fix: merge
    
    * design : 글쓰기 폼 및 수정 폼 UI 개선
    
    * feat: admin & system user만 댓글 작성 가능
    
    * feat: admin & system user 권한 체크
    
    * feature : 공개 및 비공개 글 여부에 따른 열람 권한 기능 추가
    
    * feat: 답변완료 기능
    
    * design : 글쓰기 버튼 및 글 리스트 ui 변경
    
    * design: 자물쇠 아이콘 추가
    
    * design: 버튼 추가
    
    * design : 문의 게시판 UI개선 및 버튼 위치 이동
    
    ---------
    
    Co-authored-by: jaeuklim <jeuk007@skuniv.ac.kr>
    Co-authored-by: hyojun kim <37866182+gimyojun@users.noreply.github.com>

---
+++ front/src/pages/Inquiry/Inquiry.js
@@ -0,0 +1,175 @@
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Button,
+} from "@material-ui/core";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import ReportHeader from "../Report/ReportHeader";
+import { HttpGet, HttpPost } from "../../services/HttpService";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuth } from "../../utils/AuthContext";
+import LockIcon from '@mui/icons-material/Lock'; // Lock 아이콘 임포트
+
+const Inquiry = () => {
+  const [data, setData] = React.useState({
+    totalPages: 0,
+    totalElements: 0,
+    size: 0,
+    content: [],
+    number: 0,
+  });
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const { getUserId, getUserName, isLogin } = useAuth();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    fetchData(page, rowsPerPage);
+  }, [page, rowsPerPage]);
+
+  React.useEffect(() => {
+    console.log("Data received:", data);
+  }, [data]);
+
+  // 신고 리스트 API GET
+  const fetchData = async (currentPage, pageSize) => {
+    try {
+      const response = await HttpGet(
+        `/api/v1/inquiry/list`,
+        {
+          page: currentPage,
+          pageSize: pageSize,
+        }
+      );
+
+
+      console.log("전송확인");
+      console.log("fetch data 확인", response);
+      setData(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+
+    }
+  };
+
+  const getStatusColor = (isCompleted) => {
+    return isCompleted ? "#008000" : "#FFA500";
+  };
+
+  const formatDate = (dateString) => {
+    if (!dateString) {
+      return "";
+    }
+
+    const formattedDate = new Date(dateString);
+    const year = formattedDate.getFullYear();
+    const month = String(formattedDate.getMonth() + 1).padStart(2, "0");
+    const day = String(formattedDate.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    const newRowsPerPage = parseInt(event.target.value, 10);
+    setPage(0);
+    setRowsPerPage(newRowsPerPage);
+  };
+
+  return (
+    <Box mt={4} mb={4}>
+      <ReportHeader
+        headerTitle={"1대1 문의하기"}
+        headerDescription={"1대1 문의 게시판입니다."}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>글 번호</TableCell>
+              <TableCell>상태 </TableCell>
+              <TableCell>문의 유형</TableCell>
+              <TableCell>글 제목</TableCell>
+              <TableCell>작성자</TableCell>
+              <TableCell>등록 일시</TableCell>
+              <TableCell>조회수</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.content.map((row) => (
+              <TableRow
+                key={row.id}
+                style={{
+                  textDecoration: "none",
+                }}
+                onClick={(e) => {
+                  // 조건 검사 로직
+                  console.log("공개여부:", row.isPublished);
+
+                  if (row.isPublished == false && getUserName() !== "admin" && getUserName() !== row.writer) {
+                    alert("작성자와 관리자만 볼 수 있습니다");
+                    console.log("현재 사용자:", getUserName());
+                    console.log("글 작성자:", row.writer);
+                  } else {
+                    // 조건이 충족될 때만 이동
+                    navigate(`/inquiry/${row.id}`);
+                  }
+                }}
+              >
+                <TableCell>{row.id}</TableCell>
+                <TableCell
+                  style={{
+                    color: row.inquiryState === "답변완료" ? "blue" : row.inquiryState === "답변대기" ? "red" : "black",
+                  }}
+                >{row.inquiryState}</TableCell>
+                <TableCell>{row.inquiryType}</TableCell>
+                <TableCell>
+                  {row.title}
+                  {!row.isPublished && <LockIcon fontSize="small" style={{ verticalAlign: "middle", marginRight: 5 }} />}
+                </TableCell>
+                <TableCell>{row.writer}</TableCell>
+                <TableCell>{formatDate(row.createdDate)}</TableCell>
+                <TableCell>{row.viewCount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+
+        <TablePagination
+          rowsPerPageOptions={[5, 10]}
+          component="div"
+          count={data.totalElements}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          sx={{ marginLeft: "auto" }}
+          href="/inquiry/form"
+        >
+          새 글쓰기
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Inquiry;
