5656170e2511943e113053f64e552743fa9f7293
jaeuklim <jeuk007@skuniv.ac.kr>
2024-02-14 14:17:12 +0900

    fix : 비로그인 사용자가 글쓰기 폼으로 접근할수있었던 버그 수정.

--- front/src/pages/Inquiry/Inquiry.js
+++ front/src/pages/Inquiry/Inquiry.js
@@ -158,7 +158,8 @@ const Inquiry = () => {
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
-        <Button
+        {isLogin()?(
+          <Button
           variant="contained"
           color="primary"
           size="medium"
@@ -167,6 +168,9 @@ const Inquiry = () => {
         >
           새 글쓰기
         </Button>
+
+        ):null}
+        
       </Box>
     </Box>
   );
