be76824e48b3477cb45ef783e29decc509044650
Bisi3asi <kueeng8008@naver.com>
2024-02-28 14:19:13 +0900

    feat: 충전요금 엔티티 구현, 연관관계 매핑

---
+++ src/main/java/com/ll/eitcharge/domain/chargeFee/chargeFee/entity/ChargeFee.java
@@ -0,0 +1,43 @@
+package com.ll.eitcharge.domain.chargeFee.chargeFee.entity;
+
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+import com.ll.eitcharge.domain.operatingCompany.operatingCompany.entity.OperatingCompany;
+import com.ll.eitcharge.global.jpa.entity.BaseTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class ChargeFee extends BaseTime {
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "busi_id")
+	private OperatingCompany operatingCompany;
+	private String bnm;
+
+	private String chgerType;
+
+	private Double memberFee;
+	private Double nonMemberFee;
+
+	private Double memberFeeChange;
+	private Double nonMemberFeeChange;
+
+	public void update(String chgerType, Double newMemberFee, Double newNonMemberFee, Double prevMemberFee, Double prevNonMemberFee) {
+		this.chgerType = chgerType;
+        this.memberFee = newMemberFee;
+        this.nonMemberFee = newNonMemberFee;
+        this.memberFeeChange = newMemberFee - prevMemberFee;
+        this.nonMemberFeeChange = newNonMemberFee - prevNonMemberFee;
+	}
+}
