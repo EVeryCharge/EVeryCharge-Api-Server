2c66fd577369c44ca1acc121e463ef951813dfaf
Bisi3asi <kueeng8008@naver.com>
2024-02-28 21:50:01 +0900

    feat: Null 값 조건 조회 JPQL JPA 편의 메소드 구현

--- src/main/java/com/ll/eitcharge/domain/chargeFee/chargeFee/repository/ChargeFeeRepository.java
+++ src/main/java/com/ll/eitcharge/domain/chargeFee/chargeFee/repository/ChargeFeeRepository.java
@@ -1,8 +1,10 @@
 package com.ll.eitcharge.domain.chargeFee.chargeFee.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ll.eitcharge.domain.chargeFee.chargeFee.entity.ChargeFee;
@@ -10,4 +12,9 @@ import com.ll.eitcharge.domain.chargeFee.chargeFee.entity.ChargeFee;
 @Repository
 public interface ChargeFeeRepository extends JpaRepository<ChargeFee, Long> {
 	Optional<ChargeFee> findByBnmAndChgerType(String bnm, String chgerType);
+
+	@Query("SELECT cf FROM ChargeFee cf "
+		+ "WHERE (:bnms IS NULL OR cf.bnm IN :bnms) "
+		+ "AND (:chgerType IS NULL OR cf.chgerType = :chgerType)")
+	List<ChargeFee> findAllByBnmsAndChgerType(List<String> bnms, String chgerType);
 }
