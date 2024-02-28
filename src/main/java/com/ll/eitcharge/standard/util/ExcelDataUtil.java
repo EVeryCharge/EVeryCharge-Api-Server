da54c5e6a026122234f7c0f4dc16c4197dcd9c3c
Bisi3asi <kueeng8008@naver.com>
2024-02-28 14:20:14 +0900

    feat: API 응답 → ASPOSE WORKBOOK 리턴 엑셀 유틸 클래스 구현

---
+++ src/main/java/com/ll/eitcharge/standard/util/ExcelDataUtil.java
@@ -0,0 +1,46 @@
+package com.ll.eitcharge.standard.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.aspose.cells.Workbook;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class ExcelDataUtil {
+	private final WebClient webClient;
+
+	public ExcelDataUtil() {
+		this.webClient = WebClient.builder()
+			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_OCTET_STREAM_VALUE)
+			.defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br, zstd")
+			.defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
+			.build();
+	}
+
+	public Workbook getDataByWorkbook(String httpRequestUrl) {
+		// HTTP 요청, 파일 다운로드
+		Mono<byte[]> responseMono = webClient.get()
+			.uri(httpRequestUrl)
+			.accept(MediaType.APPLICATION_OCTET_STREAM)
+			.retrieve()
+			.bodyToMono(byte[].class);
+
+		InputStream inputStream = new ByteArrayInputStream(responseMono.blockOptional().orElse(new byte[0]));
+		try {
+			return new Workbook(inputStream);
+		} catch (Exception e) {
+			log.error("ERROR : API로부터 Workbook 불러오기 실패");
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
