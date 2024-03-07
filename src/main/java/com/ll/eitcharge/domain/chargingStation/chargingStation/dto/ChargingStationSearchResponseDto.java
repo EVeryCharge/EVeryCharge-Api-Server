e2c5d6639cb0ed1ef96d61c3ec960edf3331668a
Bisi3asi <kueeng8008@naver.com>
2024-03-08 00:50:28 +0900

    fix: 충전소 지도(주변)검색 DTO 수정

--- src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/dto/ChargingStationSearchResponseDto.java
+++ src/main/java/com/ll/eitcharge/domain/chargingStation/chargingStation/dto/ChargingStationSearchResponseDto.java
@@ -1,68 +1,31 @@
 package com.ll.eitcharge.domain.chargingStation.chargingStation.dto;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import com.ll.eitcharge.domain.charger.charger.entity.Charger;
 import com.ll.eitcharge.domain.chargingStation.chargingStation.entity.ChargingStation;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ChargingStationSearchResponseDto {
     private String statId; // 충전소 ID
-    private String statNm; // 충전소 이름
-    private String addr; // 주소
-    private String useTime; // 이용 가능 시간 등
-    private double lat; // 위도
-    private double lng; // 경도
-    private List<String> chargerTypes; // 보유 충전기 타입
-    private String busiId; // 기관 ID
     private String bnm; // 기관명
-    private String parkingFree; // 무료주차 여부
-    private String limitYn; // 개방 여부
-    private String limitDetail;
-
-    // 총 충전기수, 충전가능대수 관련 DTO 엘리먼트 추가
     private String totalChger;
     private String availableChger;
+    private double lat; // 위도
+    private double lng; // 경도
 
-    // API TEST 용 필드(백업)
-    //    private String regionName;
-    //    private String regionDetailName;
-    //    private String zcode;
-    //    private String zscode;
-    //    private String isPrimary;
 
     public ChargingStationSearchResponseDto(ChargingStation chargingStation) {
         this.statId = chargingStation.getStatId();
-        this.statNm = chargingStation.getStatNm();
-        this.addr = chargingStation.getAddr();
-        this.useTime = chargingStation.getUseTime();
-        this.lat = chargingStation.getLat();
-        this.lng = chargingStation.getLng();
-        this.chargerTypes = chargingStation.getChargers().stream()
-                .map(Charger::getChgerType)
-                .distinct()
-                .collect(Collectors.toList());
-        this.busiId = chargingStation.getOperatingCompany().getBusiId();
         this.bnm = chargingStation.getOperatingCompany().getBnm();
-        this.parkingFree = chargingStation.getParkingFree();
-        this.limitYn = chargingStation.getLimitYn();
-        this.limitDetail = chargingStation.getLimitDetail();
-        // 총 충전기수, 충전가능대수 관련 DTO 엘리먼트 추가
         this.totalChger = String.valueOf(chargingStation.getChargers().size());
         this.availableChger = String.valueOf(chargingStation.getChargers().stream()
              .filter(charger -> charger.getStat().contentEquals("2"))
              .count());
-
-        // API TEST 용 필드(백업)
-        //        this.isPrimary = chargingStation.getOperatingCompany().getIsPrimary();
-        //        this.zscode = chargingStation.getRegionDetail().getZscode();
-        //        this.regionDetailName = chargingStation.getRegionDetail().getRegionDetailName();
-        //        this.zcode = chargingStation.getRegionDetail().getZcode().getZcode();
-        //        this.regionName = chargingStation.getRegionDetail().getZcode().getRegionName();
+        this.lat = chargingStation.getLat();
+        this.lng = chargingStation.getLng();
     }
 }
