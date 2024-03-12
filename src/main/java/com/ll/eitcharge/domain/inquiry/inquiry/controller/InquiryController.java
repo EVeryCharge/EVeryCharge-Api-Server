f9258f2fee272b56eff2105a90c67d96bf5921b7
jaeuklim <jeuk007@skuniv.ac.kr>
2024-03-12 16:28:22 +0900

    feat : 문의게시판 fileupload api 및 관련 로직 추가

--- src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/controller/InquiryController.java
+++ src/main/java/com/ll/eitcharge/domain/inquiry/inquiry/controller/InquiryController.java
@@ -1,5 +1,8 @@
 package com.ll.eitcharge.domain.inquiry.inquiry.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.ll.eitcharge.domain.inquiry.inquiry.dto.InquiryRequestDto;
 import com.ll.eitcharge.domain.inquiry.inquiry.dto.InquiryResponseDto;
 import com.ll.eitcharge.domain.inquiry.inquiry.entity.Inquiry;
@@ -14,24 +17,32 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
-
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
-@RequestMapping(value = "/api/v1/inquiry", produces = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/inquiry")
 @Tag(name = "InquiryController", description = "문의게시판 컨트롤러 API")
 @RequiredArgsConstructor
 public class InquiryController {
     private final InquiryService inquiryService;
+    private final AmazonS3 amazonS3;
+
+    private static String IMG_DIR_NAME = "img1";
+    private static String region = "ap-northeast-2";
+    private static String bucket = "dev-bucket-eticharge-1";
 
     @GetMapping("/list")
     public ResponseEntity<Page<InquiryResponseDto>> getList(
@@ -49,7 +60,14 @@ public class InquiryController {
             @RequestBody @Valid InquiryRequestDto requestDto,
             Principal principal
     ) {
-        inquiryService.create(requestDto, principal.getName());
+        String fileurl;
+        System.out.println("파일이름이 뭔데" + requestDto.getS3fileName());
+        if(requestDto.getS3fileName() == null)
+            fileurl = null;
+        else
+            fileurl = "https://" + bucket + ".s3." + region + ".amazonaws.com/" + requestDto.getS3fileName();
+
+        inquiryService.create(requestDto, principal.getName(), fileurl);
 
         return ResponseEntity.ok(requestDto);
     }
@@ -79,5 +97,49 @@ public class InquiryController {
         inquiryService.delete(id, principal.getName());
     }
 
+    @GetMapping("/")
+    public List<String> listBuckets() {
+        // S3 버킷 리스트를 가져옵니다.
+        List<Bucket> buckets = amazonS3.listBuckets();
+
+        // 버킷 이름만 추출하여 리스트로 반환합니다.
+        return buckets.stream().map(Bucket::getName).collect(Collectors.toList());
+    }
+
+    @PostMapping("/fileupload")
+    @ResponseBody
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName=file.getOriginalFilename();
+            // 파일을 S3에 업로드합니다.
+//        // ObjectMetadata 객체 생성 및 설정
+            ObjectMetadata metadata= new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3.putObject(bucket,fileName,file.getInputStream(),metadata);
+            return ResponseEntity.ok("업로드 완료");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("/deleteFile")
+    public String showDeleteFile() {
+        return """
+                <form action="/deleteFile" method="post">
+                    <input type="text" name="fileName">
+                    <input type="submit" value="delete">
+                </form>
+                """;
+    }
+
+    @PostMapping("/deleteFile")
+    @ResponseBody
+    public String deleteFile(String fileName) {
+        // 파일을 S3에서 삭제합니다.
+        amazonS3.deleteObject(bucket, IMG_DIR_NAME + "/" + fileName);
+
+        return "파일이 삭제되었습니다.";
+    }
 
 }
