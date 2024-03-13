92b9fc7ae56ac2cbd110f56ecc64bd4e7d66611b
i-am-jiwon <dlark1128@gmail.com>
2024-03-13 14:31:00 +0900

    feature : my 페이지 생성

--- front/src/pages/Mypage/My.js
+++ front/src/pages/Mypage/My.js
@@ -4,53 +4,59 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Help, Info } from "@material-ui/icons";
 import { Tooltip, Typography } from "@mui/material";
 import React, { useState } from "react";
-
+import { Link, useNavigate } from "react-router-dom";
+import {
+    Button,
+} from "@material-ui/core";
 const My = () => {
-  const classes = useStyles();
-  const [value, setValue] = useState(0);
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <div className={classes.root}>
-      <h1>마이페이지</h1>
-       
-       
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <h1>마이페이지</h1>
+            <Button variant="outlined" color="secondary" component={Link}
+                to="/carInit" >
+                내 차 등록
+            </Button>
+
+        </div>
+    );
 };
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    height: "100vh",
-  },
-  callout: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: "20px 0",
-    padding: "10px",
-    backgroundColor: "#f2f2f2",
-    borderRadius: "5px",
-  },
-  disclaimer: {
-    textAlign: "right",
-    fontSize: "0.8rem",
-    color: "#666",
-    marginTop: "5px",
-  },
-  tabContainer: {
-    marginTop: "20px",
-    flexGrow: 1,
-    width: "70%",
-  },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh",
+    },
+    callout: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "20px 0",
+        padding: "10px",
+        backgroundColor: "#f2f2f2",
+        borderRadius: "5px",
+    },
+    disclaimer: {
+        textAlign: "right",
+        fontSize: "0.8rem",
+        color: "#666",
+        marginTop: "5px",
+    },
+    tabContainer: {
+        marginTop: "20px",
+        flexGrow: 1,
+        width: "70%",
+    },
 }));
 
 export default My;
