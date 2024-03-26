58acb61b6e49fef9009fc5d1afd108e3db91a9f1
Bi3a <60452780+Bisi3asi@users.noreply.github.com>
2024-03-26 16:00:40 +0900

    운영 DB 데이터 워밍 (#229)
    * remove, chore: 패키지명 변경, 도메인 관련 환경변수 설정
    
    * fix: init sql 데이터 수정(불변성 데이터)
    
    * fix: 전역 업데이트 시 데이터 유효성 향상 목적 로직 수정
    
    * test: 운영 DB 워밍용 스케줄러 세팅

--- src/main/java/com/ll/everycharge/domain/charger/update/charger/scheduler/ChargerBatchUpdateScheduler.java
+++ src/main/java/com/ll/everycharge/domain/charger/update/charger/scheduler/ChargerBatchUpdateScheduler.java
@@ -28,7 +28,7 @@ public class ChargerBatchUpdateScheduler {
 	private final ChargerStateUpdateConfig chargerStateUpdateConfig;
 	private final ChargerService chargerService;
 
-	@Scheduled(cron = "0 30 3 * * *") // 운영용
+	@Scheduled(cron = "0 20 16 * * *") // 운영용
 	public void chargerBatchUpdateScheduled() {
 		while (!AppConfig.isAppInitialized || chargerStateUpdateConfig.isUpdateRunning()) {
 			try {
